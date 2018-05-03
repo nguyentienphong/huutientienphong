47bca34ddf882b2a39a623ca2d6ce2590aef623c
Tien <tiennhss@gmail.com>
2018-05-03 16:05:07 +0700

    tiennh

---
+++ home/view/home/vih_book_room.php
@@ -0,0 +1,63 @@
+<div id="colorlib-reservation">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 search-wrap">
+						<form method="post" class="colorlib-form">
+		              	<div class="row">
+		                <div class="col-md-3">
+		                  <div class="form-group">
+		                    <label for="date">Check-in:</label>
+		                    <div class="form-field">
+		                      <i class="icon icon-calendar2"></i>
+		                      <input type="text" id="date" class="form-control date" placeholder="Check-in date">
+		                    </div>
+		                  </div>
+		                </div>
+		                <div class="col-md-3">
+		                  <div class="form-group">
+		                    <label for="date">Check-out:</label>
+		                    <div class="form-field">
+		                      <i class="icon icon-calendar2"></i>
+		                      <input type="text" id="date" class="form-control date" placeholder="Check-out date">
+		                    </div>
+		                  </div>
+		                </div>
+		                <div class="col-md-2">
+		                  <div class="form-group">
+		                    <label for="adults">Adults</label>
+		                    <div class="form-field">
+		                      <i class="icon icon-arrow-down3"></i>
+		                      <select name="people" id="people" class="form-control">
+		                        <option value="#">1</option>
+		                        <option value="#">2</option>
+		                        <option value="#">3</option>
+		                        <option value="#">4</option>
+		                        <option value="#">5+</option>
+		                      </select>
+		                    </div>
+		                  </div>
+		                </div>
+		                <div class="col-md-2">
+		                  <div class="form-group">
+		                    <label for="children">Children</label>
+		                    <div class="form-field">
+		                      <i class="icon icon-arrow-down3"></i>
+		                      <select name="people" id="people" class="form-control">
+		                        <option value="#">1</option>
+		                        <option value="#">2</option>
+		                        <option value="#">3</option>
+		                        <option value="#">4</option>
+		                        <option value="#">5+</option>
+		                      </select>
+		                    </div>
+		                  </div>
+		                </div>
+		                <div class="col-md-2">
+		                  <input type="submit" name="submit" id="submit" value="Search" class="btn btn-primary btn-block">
+		                </div>
+		              </div>
+		            </form>
+					</div>
+				</div>
+			</div>
+		</div>
